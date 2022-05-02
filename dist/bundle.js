17b9c75ddbc4ad7e949d88d4895e085d23fc28dc
ZJX <1360151219@qq.com>
2022-05-02 21:21:31 +0800

    [feat]distinct env configuration

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log(2+c),document.querySelector("#app").innerHTML="Hello from your JavaScript file ..."})();
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.dev.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, './src/index.js'),// 入口
+  devtool: 'eval-source-map',
+  module: { // loader
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hello HtmlWebpackPlugin2",
+      template: path.resolve(__dirname, "./src/index.html")
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
