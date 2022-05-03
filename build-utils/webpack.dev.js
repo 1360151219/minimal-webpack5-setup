0b07f02037164fa6517f87fda5caf545f086dae4
ZJX <1360151219@qq.com>
2022-05-03 11:26:26 +0800

    [feat]add dotenv-webpack

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,12 +1,12 @@
 const { DefinePlugin } = require('webpack')
+const DotEnv = require('dotenv-webpack')
+const path = require('path')
 module.exports = {
   mode: "development",
   devtool: 'eval-source-map',
   plugins: [
-    new DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('development')
-      }
+    new DotEnv({
+      path: path.resolve(__dirname, "..", "./.env.development")
     })
   ]
 };
---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log(7),console.log("production"),document.querySelector("#app").innerHTML="Hello from your JavaScript file ..."})();
+//# sourceMappingURL=bundle.js.map
